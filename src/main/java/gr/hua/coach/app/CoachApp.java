f5b7fed397143be2df7b72aac2f63fc4287995a5
alexpapa99 <it21755@hua.gr>
2025-12-15 12:42:54 +0200

    Add CLI argument parsing skeleton

--- src/main/java/gr/hua/coach/app/CoachApp.java
+++ src/main/java/gr/hua/coach/app/CoachApp.java
@@ -1,8 +1,44 @@
 package gr.hua.coach.app;
 
+import gr.hua.coach.model.Activity;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CoachApp {
 
     public static void main(String[] args) {
-        System.out.println("Coach application started");
+        if (args.length == 0) {
+            printUsage();
+            return;
+        }
+
+        List<File> tcxFiles = new ArrayList<>();
+        Double weight = null;
+
+        for (int i = 0; i < args.length; i++) {
+            if ("-w".equals(args[i]) && i + 1 < args.length) {
+                weight = Double.parseDouble(args[++i]);
+            } else {
+                tcxFiles.add(new File(args[i]));
+            }
+        }
+
+        if (tcxFiles.isEmpty()) {
+            System.out.println("No .tcx files provided.");
+            return;
+        }
+
+        // TODO: parser + stats here
+        System.out.println("Files to parse: " + tcxFiles.size());
+        if (weight != null) {
+            System.out.println("User weight: " + weight + " kg");
+        }
+    }
+
+    private static void printUsage() {
+        System.out.println("Usage:");
+        System.out.println("java -jar coach.jar [-w weight] file1.tcx file2.tcx ...");
     }
 }
